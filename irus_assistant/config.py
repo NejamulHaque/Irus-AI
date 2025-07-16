e7630280dc4fa87e734bce5b5eaa0d5074fcb6d2
Nejamul Haque <nejamulhaque.05@gmail.com>
2025-07-16 12:03:27 +0530

    Add files via upload

--- irus_assistant/config.py
+++ irus_assistant/config.py
@@ -8,10 +8,10 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_MODEL = os.getenv("GROQ_MODEL", "llama3-70b-8192")
 
 # MySQL DB
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+HOST = os.getenv("DB_HOST")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+DATABASE = os.getenv("DB_NAME")
 
 # Flask
 FLASK_SECRET_KEY = os.getenv("SECRET_KEY")
